b8257a31e9cd6076eaadaba2bb69df67c7041d57
AnnMaryKurian <126964761+AnnMaryKurian@users.noreply.github.com>
2023-04-05 10:42:55 +0100

    Add files via upload
    code for lineplots

--- ADSAssignment2.py
+++ ADSAssignment2.py
@@ -43,11 +43,22 @@ def stat_data(dframe, col, value, yr, indi):
 
 def bar_plot(data, title, x, y):
     ax = data.plot.bar(x='Country Name', rot=0, figsize=(50,30), fontsize=50)
-    ax.set_yticks([0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80])
+    ax.set_yticks([0,20,40,60,80,100])
     ax.set_title(title.upper(), fontsize=50, fontweight = 'bold')
     ax.set_xlabel(x, fontsize=50)
     ax.set_ylabel(y, fontsize=50)
     ax.legend(fontsize=50)
+    plt.savefig(title + '.png')
+    plt.show()
+    return
+
+def line_plot(data, title, x, y):
+    data.plot.line(figsize=(50, 30), fontsize=36, linewidth=10.0)
+    plt.yticks([0, 5, 10, 15, 20])
+    plt.title(title.upper(), fontsize=50, fontweight='bold')
+    plt.xlabel(x, fontsize=50)
+    plt.ylabel(y, fontsize=50)
+    plt.legend(fontsize=50)
     plt.savefig(title + '.png')
     plt.show()
     return
@@ -68,7 +79,20 @@ bar_plot(bardata1, 'CO2 emissions from liquid fuel consumption',
 bar_plot(bardata3, 'Agricultural land ', 'Countries', 'Agricultural land')
 
 
+line_country = ['Angola', 'Ghana', 'Ethiopia', 'Brazil']
+line_year = ['1961', '1962', '1963', '1964']
+linedata1, linedata2 = datafilter(
+    data, 'Indicator Name', 'CO2 emissions from solid fuel consumption (% of total)', line_country, line_year)
+print(linedata1)
+print(linedata2)
 
+linedata3, linedata4 = datafilter(data, 'Indicator Name',
+                          'Arable land (% of land area)', line_country, line_year)
+print(linedata3)
+print(linedata4)
+line_plot(linedata2, 'CO2 emissions from solid fuel consumption',
+          'Years', 'CO2 emissions')
+line_plot(linedata4, 'Arable land', 'Years', 'Arable land')
 
 
 
